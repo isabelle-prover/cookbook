898e7cbbc065b81e3a0d96c0811c403a3c9944ab
Simon Wimmer <wimmers@in.tum.de>
2020-03-23 19:45:15 +0100

    Address TODOs and add link to isabelle-finite repository

--- src/proofs/finite/Finite.thy
+++ src/proofs/finite/Finite.thy
@@ -1,32 +1,71 @@
 theory Finite
-  imports "HOL.Finite_Set" "HOL.Relation"
+  imports Main
 begin
 
 
-section \<open>Simproc @{verbatim finite_Collect}\<close>
+section \<open>Simproc \<^verbatim>\<open>finite_Collect\<close>\<close>
 text \<open>
-  The simproc @{verbatim finite_Collect} can be used to solve certain finiteness proof oblgations.
-  TODO: What kind of proof obligations exactly?
+  The simproc \<^verbatim>\<open>finite_Collect\<close> can be used to solve certain finiteness proof oblgations.
+  Essentially, the simproc attempts to rewrite set comprehensions to terms that consist
+  of applications @{term image}, @{term vimage}, @{term "(\<inter>)"},
+  and @{term "(\<times>)"} as far as possible.
   Below is an example from @{theory HOL.Relation}.
-  TODO: Why is the simproc important here?
 \<close>
-
 lemma "finite (r\<inverse>) = finite r"
-unfolding converse_def conversep_iff using [[simproc add: finite_Collect]]
+  unfolding converse_def conversep_iff using [[simproc add: finite_Collect]]
+  apply simp \<comment> \<open>The set was rewritten to an application of @{term image}.\<close>
   by (auto elim: finite_imageD simp: inj_on_def)
 
-section \<open>Lemmas for Proving Finiteness of Sets\<close>
+section \<open>Lemmas For Proving Finiteness of Sets\<close>
 text \<open>
-  For automated proofs, it can often help to just pass these with the @{verbatim intro} modifier to @{method auto} and friends.
-  TODO: Why and in which cases?
-  TODO: Add example.
+  For automated proofs, it can often help to just pass these with the \<^verbatim>\<open>intro\<close> modifier
+  to @{method auto} and friends.
+  The theorem @{thm finite_imageI} is set up as an default, but it can help to add \<^verbatim>\<open>intro!\<close>.
+  Have a look at the example of the end of the file, to see both theorems in action.
 \<close>
 lemmas finite_helpers = finite_imageI finite_subset
 
-section \<open>Tuning the Simplifier for Finiteness Proofs\<close>
+
+section \<open>Tuning The Simplifier For Finiteness Proofs\<close>
 subsection \<open>Rewriting to Images\<close>
 text \<open>
-  One useful technique is to rewrite set comprehensions to images of sets under functions as far as possible by passing @{thm setcompr_eq_image} to the simplifier.
-  TODO: Add an example.
+  One useful technique is to rewrite set comprehensions to images of sets under functions as far as
+  possible by passing @{thm setcompr_eq_image} or its \<open>n\<close>-ary generalizations to the simplifier.
 \<close>
+
+text \<open>Here is the \<open>2\<close>-ary generalization of @{thm setcompr_eq_image}.\<close>
+lemma setcompr_eq_image2:
+  "{f x y | x y. P x y} = (\<lambda>(x, y). f x y) ` {(x, y). P x y}"
+  using setcompr_eq_image[where f = "(\<lambda>(x, y). f x y)" and P = "\<lambda>(x, y). P x y"] by simp
+
+text \<open>In this example, \<^verbatim>\<open>finite_Collect\<close> still does the right thing:\<close>
+lemma
+  assumes "finite A"
+  shows "finite {f x | x. x \<in> A \<and> P x}"
+  using assms [[simproc add: finite_Collect]] by simp
+
+text \<open>Here, however, it will produce quite a mess:\<close>
+lemma
+  assumes "finite (A \<times> B)"
+  shows "finite {f x y | x y. x \<in> A \<and> y \<in> B \<and> R x y}"
+  using assms [[simproc add: finite_Collect]]
+  apply simp
+  oops
+
+text \<open>Instead, we use @{thm setcompr_eq_image2} to an image:\<close>
+lemma
+  assumes "finite (A \<times> B)"
+  shows "finite {f x y | x y. x \<in> A \<and> y \<in> B \<and> R x y}"
+  using assms
+  apply (simp add: setcompr_eq_image2) \<comment> \<open>Looks much better.\<close>
+  \<comment> \<open>\<^verbatim>\<open>auto\<close> will apply the following theorems to solve the goal:\<close>
+  thm finite_imageI finite_subset[rotated]
+  \<comment> \<open>We rotate @{thm finite_subset} to use it as an \<open>elim\<close>-rule\<close>
+  apply (auto elim: finite_subset[rotated])
+  done
+
+section \<open>Further Pointers\<close>
+text \<open>For more examples and an attempt at an proof method for finiteness proof obligations,
+see \<^url>\<open>https://github.com/wimmers/isabelle-finite\<close>.\<close>
+
 end
